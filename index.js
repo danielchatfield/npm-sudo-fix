23be17b8f1148dbcc4e8c4211d625cf7db313342
Daniel Chatfield <chatfielddaniel@gmail.com>
2014-12-08 23:19:33 +0000

    Don't just guess home directory

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 'use strict';
 
+if(process.platform == 'win32') {
+    console.log("This seems to windows, you mad man.");
+    process.exit();
+}
+
 var spawny = require("spawny");
 
-var username = process.env['USER'];
+var username = process.env.USER;
 
 spawny("sudo chown -R " + username + " /usr/local/lib/node_modules");
 
-spawny("sudo chown -R " + username + " /users/" + username + "/.npm");
+spawny("sudo chown -R " + username + process.env.HOME + "/.npm");
